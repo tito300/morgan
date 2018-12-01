13ece60d0f52d0b99ef3e18a1c59f1bf3ab19414
tito300 <tarek.demachkie@gmail.com>
2018-12-01 13:30:24 -0600

    switched to template literals

--- index.js
+++ index.js
@@ -376,16 +376,16 @@ function compile (format) {
   }
 
   var fmt = String(JSON.stringify(format))
-  var js = '  "use strict"\n  return ' + fmt.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function (_, name, arg) {
+  var js = `  "use strict"\n  return ${fmt.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function (_, name, arg) {
     var tokenArguments = 'req, res'
-    var tokenFunction = 'tokens[' + String(JSON.stringify(name)) + ']'
+    var tokenFunction = `tokens[${String(JSON.stringify(name))}]`
 
     if (arg !== undefined) {
-      tokenArguments += ', ' + String(JSON.stringify(arg))
+      tokenArguments += `, ${String(JSON.stringify(arg))}`
     }
 
-    return '" +\n    (' + tokenFunction + '(' + tokenArguments + ') || "-") + "'
-  })
+    return `" +\n    (${tokenFunction}(${tokenArguments}) || "-") + "`
+  })}`
 
   // eslint-disable-next-line no-new-func
   return new Function('tokens, req, res', js)
